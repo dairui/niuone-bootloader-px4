d119f94774f1a91d689020f36236fc4ba017e94a
David Sidrane <david_s5@nscdg.com>
2016-08-05 12:54:14 -1000

    Added PX4 IO V3

--- cdcacm.c
+++ cdcacm.c
@@ -42,7 +42,7 @@
 #include <libopencm3/usb/cdc.h>
 
 #include "bl.h"
-
+#if INTERFACE_USB != 0
 #define USB_CDC_REQ_GET_LINE_CODING			0x21 // Not defined in libopencm3
 
 
@@ -374,3 +374,4 @@ usb_cout(uint8_t *buf, unsigned count)
 		}
 	}
 }
+#endif
